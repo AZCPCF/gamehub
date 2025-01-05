b97efd87d419890b6a2ee7969ef42e6bf913b499
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-06 00:52:34 +0330

    add zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
-import Book from "./components/Book";
 import Books from "./components/Books";
+import { useCounter } from "./store/useCounter";
 
 function App() {
+  const {count,dec,inc} = useCounter()
   return (
+    <>
+    <div className="flex justify-center items-center w-full h-20">
+      <button onClick={dec}>-</button>
+      <h1 className="p-4">{count}</h1>
+      <button onClick={inc}>+</button>
+    </div>
     <div className="flex flex-wrap m-4">
       <Books />
     </div>
+    </>
   );
 }
 
