882b89304076d3a113dc9b404a16e0bfcc3ddc94
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-06 03:00:03 +0330

    add disabled props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,17 +4,19 @@ interface ButtonProps {
     text: string;
     onClick?: () => void;
     variant?: 'primary' | 'secondary';
+    disabled?:boolean;
+    className?:string;
   }
   
-  const Button: FC<ButtonProps> = ({ text, onClick, variant = 'primary' }) => {
-    const baseStyles = 'px-4 py-2 rounded font-medium focus:outline-none';
+  const Button: FC<ButtonProps> = ({ text, onClick, variant = 'primary',disabled=false }) => {
+    const baseStyles = 'w-full my-2 px-4 py-2 rounded font-medium focus:outline-none';
     const variantStyles =
       variant === 'primary'
         ? 'bg-blue-500 text-white hover:bg-blue-600'
         : 'bg-gray-200 text-gray-800 hover:bg-gray-300';
   
     return (
-      <button className={`${baseStyles} ${variantStyles}`} onClick={onClick}>
+      <button disabled={disabled} className={`${baseStyles} ${variantStyles}`} onClick={onClick}>
         {text}
       </button>
     );
