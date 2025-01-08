580db9c875d2a411c497982b62a34acf611cd73b
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-09 02:03:54 +0330

    add generics & utility types docs
    week1 -> day2 -> ts -> generics & utility  types docs

---
+++ src/ts-zod/docs/generics&utilityTypes.ts
@@ -0,0 +1,119 @@
+///// Generics
+
+function identity<Type>(arg: Type): Type {
+  return arg;
+}
+
+// 2
+
+function identity1<Type>(arg: Type): Type {
+  return arg;
+}
+let myIdentity1: GenericIdentityFn<string> = identity;
+
+// 3
+
+interface GenericIdentityFn<Type> {
+  (arg: Type): Type;
+}
+
+function identity2<Type>(arg: Type): Type {
+  return arg;
+}
+let myIdentity2: GenericIdentityFn<number> = identity;
+
+///// Utility Types
+
+// Awaited
+
+type user = {
+  name: string;
+};
+type FetchData = Awaited<Promise<number>>;
+
+async function getData(data: FetchData): Promise<number> {
+  return await data;
+}
+// Partial
+
+type PartialData = {
+  title: string;
+  id: number;
+  desc: string;
+};
+function updateData(
+  data: PartialData,
+  updateField: Partial<PartialData>
+): Partial<PartialData> {
+  return { ...data, ...updateData };
+}
+const data: PartialData = {
+  desc: "test",
+  id: 12,
+  title: "test",
+};
+
+const data2: Partial<PartialData> = updateData(data, { desc: "test" });
+
+// Required
+
+type numbers = {
+  one?: number;
+  two?: number;
+};
+const logNumbers: (data: Required<numbers>) => Required<numbers> = (data) => {
+  return data;
+};
+const nums: Required<numbers> = { one: 12, two: 34 };
+console.log(logNumbers(nums));
+
+// Readonly
+
+interface User {
+  name: string;
+}
+const userInfo: Readonly<User> = { name: "test" };
+// userInfo.name ='mmd'
+console.log(userInfo.name);
+
+// Record
+
+type CatName = "miffy" | "boris" | "mordred";
+interface CatInfo {
+  age?: number;
+  breed?: string;
+}
+const cats: Partial<Record<CatName, CatInfo>> = {
+  miffy: { age: 12 },
+  boris: { breed: "test" },
+};
+console.log(cats);
+
+// Pick
+
+interface Todo {
+  title: string;
+  desc: string;
+  isFinished: boolean;
+}
+type UserTodo = Pick<Todo, "isFinished" | "desc">;
+const userTodo: UserTodo = { isFinished: true, desc: "test" };
+console.log(userTodo.isFinished);
+
+// Omit
+
+interface BookInterface {
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: number;
+}
+
+type BookPreview = Omit<BookInterface, "title" | "completed">;
+const book: BookPreview = {
+  // title: "test",
+  // completed: true,
+  createdAt: 120,
+  description: "test",
+};
+console.log(book)
