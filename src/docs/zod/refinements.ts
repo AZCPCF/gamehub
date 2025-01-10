80641c253e18a39d8364bf24646f2018bfea92a7
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-11 02:00:34 +0330

    remove wrong docs from docs/zod

--- src/docs/zod/refinements.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-// Custom refinement
-const positiveNumber = z
-  .number()
-  .refine((val) => val < 0, { message: "number must be nonpositive" });
-
-console.log(positiveNumber.parse(10)); // Throws error
-console.log(positiveNumber.parse(-5)); // -5
