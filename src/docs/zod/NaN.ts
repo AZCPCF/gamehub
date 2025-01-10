0ca4247f1de755329c0f2d9700b7d593420d443b
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-10 19:44:09 +0330

    Create NaN.ts

---
+++ src/docs/zod/NaN.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+const isNaN = z.nan({
+  required_error: "isNaN is required",
+  invalid_type_error: "isNaN must be 'not a number'",
+});
+console.log(isNaN.parse(12)); // error
+console.log(isNaN.parse(12*'n')); // NaN
