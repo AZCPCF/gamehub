8745ae42f1132ea2f8595f8048b70745a9ecf515
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-10 17:18:55 +0330

    Create times.ts

---
+++ src/docs/zod/times.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const time = z.string().time({/*precision:3*/});
+
+time.parse("00:00:00"); // pass
+time.parse("09:52:31"); // pass
+time.parse("23:59:59.9999999"); // pass (arbitrary precision)
+
+time.parse("00:00:00.123Z"); // fail (no `Z` allowed)
+time.parse("00:00:00.123+02:00"); // fail (no offsets allowed)
