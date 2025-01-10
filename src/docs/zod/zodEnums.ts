8b08c18b3f37131310d8cc2a01b39bd6695d365a
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-10 21:24:51 +0330

    Create zodEnums.ts

---
+++ src/docs/zod/zodEnums.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+const ENUM = ["banana", "apple", "orange"] as const; // tuple
+
+const fruitEnum = z.enum(ENUM /* or ['banana','apple','orange'] */); // enum
+
+type Fruit = z.infer<typeof fruitEnum>; // 'banana' | 'apple' | 'orange'
+const fruit: Fruit = "apple";
+console.log(fruit); // apple
+console.log(fruitEnum.enum.banana); // 'banana'
+console.log(fruitEnum.options); // ['banana','apple','orange']
+
+const bananaAndApple = fruitEnum.exclude(["orange"]); // exclude
+type BananaAndApple = z.infer<typeof bananaAndApple>; // 'apple' | 'banana'
+const fruitWithoutOrange: BananaAndApple = "banana";
+console.log(fruitWithoutOrange); // 'banana'
+console.log(fruitEnum.Enum);
+
+const justOrange = fruitEnum.extract(["orange"]); // extract
+type JustOrange = z.infer<typeof justOrange>; // 'orange'
+const fruitWithoutOrangea: JustOrange = "orange";
+console.log(fruitWithoutOrangea); // 'orange'
