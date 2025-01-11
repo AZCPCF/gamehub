870eb2869d4996136de69a06907c1e81f260c6fb
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-12 02:29:44 +0330

    Create nullables.ts

---
+++ src/docs/zod/nullables.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+const nullableString = z.nullable(z.string()); // first way
+
+console.log(nullableString.parse(124)) // error
+console.log(nullableString.parse(undefined)) // error
+console.log(nullableString.parse("asdf")) // asdf
+console.log(nullableString.parse(null)) // null
+
+const nullableUser = z.object({username:z.string().nullable()}) // second way
+type NullableUser = z.infer<typeof nullableUser>
+
+const user:NullableUser = {username:null}
+const user1:NullableUser = {username:"test"}
+const user2:NullableUser = {username:undefined}
+console.log(user2) // error
+console.log(user1) // { username : "test" }
+console.log(user) // { username : null }
+
+
+const strSchema = z.string();
+const nullableStr = strSchema.nullable();
+
+/* */
+console.log(nullableStr.unwrap() == strSchema); // true, we can use unwrap see data when its required
+/* */
+
+console.log(nullableStr.unwrap().parse(null)); // error
+console.log(nullableStr.parse(null)); // null
+console.log(nullableStr.unwrap().parse("test")); // "test"
+console.log(nullableStr.unwrap().parse("test")); // "test"
