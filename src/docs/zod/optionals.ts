9607e1d3435187479f445bdf3b68c1889492423a
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-12 02:15:17 +0330

    Create optionals.ts

---
+++ src/docs/zod/optionals.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+const stringSchema = z.optional(z.string()); // first way
+
+console.log(stringSchema.parse(123)); // error
+console.log(stringSchema.parse(null)); // error
+console.log(stringSchema.parse(undefined)); // undefined
+console.log(stringSchema.parse("test")); // "test"
+
+const userAge = z.object({ age: z.number().optional() }); // second way
+type UserAge = z.infer<typeof userAge>
+
+const user:UserAge = {age:12};
+const user1:UserAge = {age:undefined};
+const user2:UserAge = {age:"test"};
+console.log(user2) // error
+console.log(user1) // { age : undefined }
+console.log(user) // { age : 12 }
+
+const strSchema = z.string();
+const optionalString = strSchema.optional();
+
+/* */
+console.log(optionalString.unwrap() == strSchema); // true, we can use unwrap see data when its required
+/* */
+
+console.log(optionalString.unwrap().parse(undefined)); // error
+console.log(optionalString.parse(undefined)); // undefined
+console.log(optionalString.unwrap().parse("test")); // "test"
+console.log(optionalString.unwrap().parse("test")); // "test"
