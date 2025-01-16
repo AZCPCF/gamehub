96486debb5f97b411aaa22a3b22b97d8382ae30a
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-17 00:51:39 +0330

    change atom folder structure

--- pixel/src/components/DinoCreator/Atom/index.tsx
+++ pixel/src/components/DinoCreator/Atom/index.tsx
@@ -1,11 +1,11 @@
 import { FC, useContext } from "react";
-import { ColorContext } from "../../contexts/ColorContext";
+import { ColorContext } from "../../../contexts/ColorContext";
 
 const Atom: FC = () => {
   const { color } = useContext(ColorContext);
   return (
     <div
-      className={`w-[10px] h-[10px] ${color}`}
+      className={`w-[5px] h-[5px] ${color}`}
       style={{ backgroundColor: color }}
     ></div>
   );
