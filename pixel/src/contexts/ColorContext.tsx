8e808ca12c7c0622205f97396b34248adf28792e
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-16 20:59:38 +0330

    Update ColorContext.tsx

--- pixel/src/contexts/ColorContext.tsx
+++ pixel/src/contexts/ColorContext.tsx
@@ -2,11 +2,13 @@ import { createContext, FC, PropsWithChildren, useState } from "react";
 
 export const ColorContext = createContext({
   color: "",
-  setColor: (color: string) => {},
+  setColor: (color: string) => {
+    console.log(color);
+  },
 });
 export const ColorContextProvider: FC<PropsWithChildren> = ({ children }) => {
   const [color, setColor] = useState("#ef4444");
-  return (  
+  return (
     <ColorContext.Provider value={{ color, setColor }}>
       {children}
     </ColorContext.Provider>
