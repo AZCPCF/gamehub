07b30c1f929f7dd7da578634abfc12c660c8d631
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-17 00:53:49 +0330

    use selectors

--- pixel/src/App.tsx
+++ pixel/src/App.tsx
@@ -1,42 +1,14 @@
-import { FC, useContext, useState } from "react";
+import { FC } from "react";
 import "./App.css";
-import { colors } from "./constants/colors";
-import { ColorContext } from "./contexts/ColorContext";
+import DinoColorSelector from "./components/DinoColorSelector";
 import Dino from "./shared/Dino";
 import { Container } from "./Styles";
 const App: FC = () => {
-  const [color, setColor] = useState("red");
-  const [colorCode, setColorCode] = useState("500");
-  const { setColor: setColorContext } = useContext(ColorContext);
+  // const din
   return (
     <>
-      <div className="absolute top-0 left-0">
-        <select
-          className="w-20 m-2"
-          value={color}
-          onChange={(e) => {
-            setColorContext(colors[e.target.value][parseInt(colorCode)]);
-            setColor(e.target.value);
-          }}
-        >
-          {Object.entries(colors).map((item) => (
-            <option value={item[0]}>{item[0]}</option>
-          ))}
-        </select>
-        <select
-          className="w-20 m-2"
-          value={colorCode}
-          onChange={(e) => {
-            setColorContext(colors[color][e.target.value]);
-            setColorCode(e.target.value);
-          }}
-        >
-          {Object.entries(colors[color]).map((item) => (
-            <option value={item[0]}>{item[0]}</option>
-          ))}
-        </select>
-      </div>
-      <Container>
+      <DinoColorSelector />
+      <Container className="h-screen w-full transition-colors duration-500">
         <Dino />
       </Container>
     </>
