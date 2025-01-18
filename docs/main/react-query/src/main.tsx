0fb083554f786ae0fcf1acb7258f5fa523fe8cb4
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-19 00:07:11 +0330

    add react-query provider in react-query app in docs

--- docs/main/react-query/src/main.tsx
+++ docs/main/react-query/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+const reactQueryClient = new QueryClient();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={reactQueryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
