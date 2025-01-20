b7f472a1e294575b4f6c9e8e8cb0d4d0740e52f3
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-20 16:27:17 +0330

    Create QueryOptions.tsx

---
+++ docs/main/react-query/src/components/QueryOptions.tsx
@@ -0,0 +1,36 @@
+import { queryOptions, useQuery } from "@tanstack/react-query";
+import { FC } from "react";
+
+const QueryOptions: FC = () => {
+  const todosOption = (id: number) =>
+    queryOptions({
+      queryKey: ["todos", id],
+      // placeholderData:{title:'test'},
+      queryFn: async () => {
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/todos/${id}`
+        );
+        if (res.ok) {
+          return await res.json();
+        }
+      },
+    });
+  const { data: todos, status, error } = useQuery(todosOption(4));
+
+  if (status == "pending") {
+    return "pending...";
+  }
+  if (status == "error") {
+    return error?.message;
+  }
+  console.log(todos);
+  return (
+    <>
+      <h1>todos</h1>
+      <p>
+        {todos.title} <span> - {!todos.completed && "un"}completed</span>
+      </p>
+    </>
+  );
+};
+export default QueryOptions;
